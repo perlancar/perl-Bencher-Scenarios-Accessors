ba358c0e91a4cb9d74c1c15561f202f9961b7b65
perlancar (@pc-office) <perlancar@gmail.com>
2016-03-31 02:20:35 +0700

    Mention Benchmark::Perl::Formance & Benchmark::Perl::Formance::Plugin::Accessors*

--- lib/Bencher/Scenarios/Accessors.pm
+++ lib/Bencher/Scenarios/Accessors.pm
@@ -5,3 +5,8 @@ package Bencher::Scenarios::Accessors;
 
 1;
 # ABSTRACT: Scenarios to benchmark class accessors
+
+=head1 SEE ALSO
+
+The L<Benchmark::Perl::Formance> distribution contains various benchmarks, in
+particular the C<Benchmark::Perl::Formance::Plugin::Accessors*> modules.
